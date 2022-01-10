796add23d0d4d669a7828aae460cd69582b84c39
weisrc <weihengjiang64@hotmail.com>
2022-01-09 21:01:35 -0500

    more ci

--- scripts/jestJsonReporter.ts
+++ scripts/jestJsonReporter.ts
@@ -1,5 +1,5 @@
-// DO NOT ADD TYPESCRIPT STUFF HERE BECAUSE IT IS READ AS JS BY JEST
 module.exports = (data) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   require("fs").writeFileSync(
     "./report/results.json",
     JSON.stringify(data)
